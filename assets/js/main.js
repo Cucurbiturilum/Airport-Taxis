e2fd7b24165d4ceb9d0203a0b619fd6e0df50b7c
cucurbiturilum <cucurbiturilum@gmail.com>
2020-03-03 00:11:00 +0200

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+$(function () {
+    $(".datepicker-input").datepicker({dateFormat: 'dd.mm.yy'});
+});
+$(function () {
+    $(".timepicker-input").timepicker({
+        interval: 5,
+    });
+});
+
+$(document).ready(function () {
+    $('.card-deck').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        navText: ['<span aria-label="Previous"><i class="fas fa-chevron-left"></i></span>', '<span aria-label="Next"><i class="fas fa-chevron-right"></i></span>'],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true
+            },
+            720: {
+                items: 2,
+                nav: false,
+                dots: true
+            },
+            1200: {
+                items: 3,
+                nav: true,
+                loop: true,
+                dots: false
+            }
+        }
+    });
+    $('.vehicles-carousel').owlCarousel({
+        loop: true,
+        navText: ['<span aria-label="Previous"><i class="fas fa-chevron-left"></i></span>', '<span aria-label="Next"><i class="fas fa-chevron-right"></i></span>'],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true
+            },
+            768: {
+                items: 1,
+                stagePadding: 150,
+                nav: false,
+                dots: true
+            },
+            990: {
+                items: 1,
+                stagePadding: 200,
+                nav: true,
+                dots: false
+            },
+            1200: {
+                items: 1,
+                nav: true,
+                dots: false,
+                stagePadding: 350
+            }
+        }
+    })
+    $('.top-destinations-carousel').owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true
+    });
+});
+
+
+
+
+
+
+
