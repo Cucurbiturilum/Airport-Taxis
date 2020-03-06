cba146fa3f8117aef1df83a01a696bdc020def77
cucurbiturilum <cucurbiturilum@gmail.com>
2020-03-06 04:35:56 +0200

    added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,53 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: [
         './assets/js/index.js',
+        './assets/css/main.scss'
     ],
     output: {
         filename: './bundle.js'
     },
     devtool: "source-map",
     module: {
-        rules: [{
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'assets/js'),
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: 'env'
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'assets/js'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 'env'
+                    }
                 }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                loader: ExtractTextPlugin.extract(
+                    'css-loader!sass-loader' // loaders to preprocess CSS
+                )
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|webp)$/i,
+                loaders: [
+                    'file-loader',
+                    'webp-loader'
+                ]
             }
-        },
         ]
     },
     plugins: [
@@ -28,5 +56,6 @@ module.exports = {
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
+        new ExtractTextPlugin('[name].css'),
     ],
 };
